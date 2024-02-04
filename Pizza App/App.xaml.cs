f6e2c60b48f396f61819571ca6991ce95ee993cc
IHC(MohammadAminFahimi) <fahimi13823182@gmail.com>
2024-02-04 20:42:17 +0330

    Add project files.

---
+++ Pizza App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pizza_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Pizza App/Pages/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Pizza_App.Pages;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
