6cc891a40e48bf113d6ab5c890067874b0075e2e
IHC(MohammadAminFahimi) <aliakhlaqi9898@gmail.com>
2024-02-06 15:49:46 +0330

    on back button in DetailPage have problem in android, other functionality is ok just bottom border not done

--- Pizza App/Pages/DetailPage.xaml.cs
+++ Pizza App/Pages/DetailPage.xaml.cs
@@ -1,9 +1,28 @@
+#if IOS
+using UIKit;
+#endif
 namespace Pizza_App.Pages;
 
 public partial class DetailPage : ContentPage
 {
-	public DetailPage()
+	private readonly DetailsViewModel _detailsViewModel;
+	public DetailPage(DetailsViewModel detailsViewModel)
 	{
+		_detailsViewModel = detailsViewModel;
 		InitializeComponent();
+		BindingContext = _detailsViewModel;
 	}
+    protected override void OnAppearing()
+    {
+#if IOS
+		base.OnAppearing();
+		var bottom = UIApplication.SharedApplication.Delegate.GetWindow().SafeAreaInsets.Bottom;
+		bottomBox.Margin = new Thickness(-1, 0, -1, (bottom + 1) * -1);
+#endif
+	}
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+		await Shell.Current.GoToAsync("..",animate:true);
+    }
 }
