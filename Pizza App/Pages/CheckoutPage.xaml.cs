0f0f107c6fbe694fa521a26a9024fbeb7fe86d69
IHC(MohammadAminFahimi) <fahimi13823182@gmail.com>
2024-02-07 18:09:41 +0330

    check out page done with hand made animation

--- Pizza App/Pages/CheckoutPage.xaml.cs
+++ Pizza App/Pages/CheckoutPage.xaml.cs
@@ -6,4 +6,27 @@ public partial class CheckoutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        img.ScaleTo(1.5);
+        msg.ScaleTo(1);
+
+        await img.ScaleTo(0.5);
+        await img.ScaleTo(1.5);
+        await img.ScaleTo(0.5);
+        await img.ScaleTo(1.5);
+        await img.ScaleTo(0.5);
+        await img.ScaleTo(1.5);
+        await img.ScaleTo(1);
+
+        homebtn.FadeTo(1,length:500);
+        await homebtn.ScaleTo(1);
+    }
+
+    private async void homebtn_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(HomePage)}",animate:true);
+    }
 }
